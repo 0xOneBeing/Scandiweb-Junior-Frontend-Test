6da82e81a5707d0ed2da4abccd08888528d2f567
Sunday Obinna Oruwhone <sundayamosobi@gmail.com>
2022-05-21 05:31:19 +0100

    Fixed currency switcher and updated site favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import logo from '../images/logo.png'
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-md bg-light navbar-light .justify-content-center text-center">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#siteNavbar">
-                <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#siteNavbar">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="siteNavbar">
+            <div className="collapse navbar-collapse" id="siteNavbar">
                 <div className="navbar-nav links col-md-4">
                     <li className="nav-item"><a className="nav-link" href="#">Women</a></li>
                     <li className="nav-item"><a className="nav-link" href="#">Men</a></li>
@@ -18,7 +18,11 @@ function Navbar() {
                     </a>
                 </div>
                 <div className="actions col-md-4">
-                    <img src="" alt="$" />
+                    <select className="currency" id='currency'>
+                        <option value="usd">$ USD</option>
+                        <option value="eur">€ EUR</option>
+                        <option value="jpy">¥ JPY</option>
+                    </select>
                 </div>
             </div>
         </nav>

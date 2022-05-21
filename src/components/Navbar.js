906e6167b89da968e36d54e5117511f03677726e
Sunday Obinna Oruwhone <sundayamosobi@gmail.com>
2022-05-21 06:39:02 +0100

    Fixed currency switcher

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,11 +18,13 @@ function Navbar() {
                     </a>
                 </div>
                 <div className="actions col-md-4">
-                    <select className="currency" id='currency'>
-                        <option value="usd">$ USD</option>
-                        <option value="eur">€ EUR</option>
-                        <option value="jpy">¥ JPY</option>
-                    </select>
+                    <div className='currencySwitcher'>
+                        <select className="currency" name="currency" id='currency'>
+                            <option value="usd">$ USD</option>
+                            <option value="eur">€ EUR</option>
+                            <option value="jpy">¥ JPY</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </nav>
